f7cb3e79e0881730027e52bb0ea87b4b05117417
FrankTheTank <franklin@bitsmasher.net>
2018-06-25 22:30:16 -0400

    june 2018 updates

--- bsidesindy/index.php
+++ bsidesindy/index.php
@@ -1,45 +1,14 @@
-<?php
-
-?><!DOCTYPE html>
-<html>
+CTYPE html>
+<html lang="en">
 <head>
-  <meta charset="utf-8">
-
-  <title>BSides Indy</title>
-  <meta name="description" content="The @indybsides website">
-  <meta name="author" content="thedevilsvoice" >
-
-  <link href="style.css" rel="stylesheet" type="text/css" />
-
-  <!--[if lt IE 9]>
-  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
+<meta name="generator" content="HTML Tidy for Linux (vers 25 March 2009), see ww
+w.w3.org">
+<meta charset="utf-8">
+<title>@indybsides</title>
 </head>
 <body>
-<div id="container">
-  <div id="header">
-    <div align="center"><img src="images/BSidesIndy_Skyline_lowres.png" alt="IndySkyline" width="510" height="215" border="0" /></div>
-  </div>
-  <?php include "nav_bar.php"; ?>
-
-  <div id="content">
-    <div id="homeText">
-	  <p align="left"><strong>Saturday, March 10, 2018</strong><br />
-	  Speakers, CTF, Lockpick Village</p>
-	  <p align="left"><strong>Location:</strong><br />
-	     <strong>New Location this year: IUPUI</strong><br />
-        <a href="https://bsidesindy2018.busyconf.com/bookings/new" target="_blank"><strong>Purchase Tickets</a></strong><br /> </p>
-        <p align="left"><strong><a href="https://imgur.com/a/w0UnE">Click here for pictures from 2017</a></strong></p>
-        <p align="left"><strong><a href="https://www.youtube.com/playlist?list=PLNhlcxQZJSm-LJeAbcp9UuKCwl9le-j9r">Click here for videos from 2017</a></strong></p>
-    </div>
-    <div id="contentPhoto">
-      <div align="right"><br /><br /><br /><br />
-         <img src="images/BSidesIndy_Logo_lowres.png" alt="flag" width="275" /></div>
-    </div>
-  </div>
-
-<?php include "footer.php"; ?>
-
-</div>
+Please check our twitter for more details while we redo our site. 
+<p>
+<a href="https://twitter.com/indybsides">https://twitter.com/indybsides</a>
 </body>
 </html>
