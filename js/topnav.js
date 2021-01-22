b8596e1fb31fab6292b4ac039a18d455aa9495e5
FrankTheTank <franklin@bitsmasher.net>
2021-01-22 15:55:59 -0500

    update topnav

--- js/topnav.js
+++ js/topnav.js
@@ -2,8 +2,9 @@ const template = document.createElement('template');
 template.innerHTML = `
 <div class="topnav">
 <a href="https://www.bitsmasher.net/bsidesindy/index.html">BSidesIndy</a>
+<a href="https://github.com/thedevilsvoice/homelab">HomeLab</a>
 <a href="https://www.bitsmasher.net/minecraft/index.html">Minecraft</a>
-<a href="https://www.bitsmasher.net/netlab/index.html">HomeLab</a>
+<a href="https://www.bitsmasher.net/netlab/index.html">NetLab</a>
 <a href="https://www.bitsmasher.net/projects/index.html">Projects</a>
 <a href="https://www.bitsmasher.net/sara2001conf/index.html">Radio Astronomy</a>
 </div>
