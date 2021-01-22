a1188acd03e8c69a17ee0d8e21b34ed9c80fd41c
FrankTheTank <franklin@bitsmasher.net>
2021-01-22 15:49:47 -0500

    test javascript templates

---
+++ js/topnav.js
@@ -0,0 +1,12 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<div class="topnav">
+<a href="https://www.bitsmasher.net/bsidesindy/index.html">BSidesIndy</a>
+<a href="https://www.bitsmasher.net/minecraft/index.html">Minecraft</a>
+<a href="https://www.bitsmasher.net/netlab/index.html">HomeLab</a>
+<a href="https://www.bitsmasher.net/projects/index.html">Projects</a>
+<a href="https://www.bitsmasher.net/sara2001conf/index.html">Radio Astronomy</a>
+</div>
+`
+
+document.body.appendChild(template.content);
