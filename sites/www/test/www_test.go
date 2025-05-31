38ff6c83e30f095bb4e8ddcbd63654dc7a4f989c
Franklin D. <2730246+devsecfranklin@users.noreply.github.com>
2025-05-31 09:04:41 -0600

    web site updates (#126)
    * update nginx configuration
    
    * nginx config update
    
    * nginx config update

---
+++ sites/www/test/www_test.go
@@ -0,0 +1,24 @@
+// +build unit,!integration
+
+package test
+
+import (
+        "testing"
+
+        "github.com/gruntwork-io/terratest/modules/terraform"
+        "github.com/stretchr/testify/assert"
+)
+
+func TestTerraformHelloWorldExample(t *testing.T) {
+        // retryable errors in terraform testing.
+        terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+                TerraformDir: "unit/terraform-hello-world-example",
+        })
+
+        defer terraform.Destroy(t, terraformOptions)
+
+        terraform.InitAndApply(t, terraformOptions)
+
+        output := terraform.Output(t, terraformOptions, "hello_world")
+        assert.Equal(t, "Hello, World!", output)
+}
