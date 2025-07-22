c7991ca050632af8eb4f218892d19e7990e98350
Franklin D. <2730246+devsecfranklin@users.noreply.github.com>
2025-07-22 16:47:09 -0600

    games; testing (#183)
    * dst page updates
    
    * update db
    
    * add minecraft mod downloader

---
+++ internal/minecraft/minecraft.go
@@ -0,0 +1,9 @@
+
+// check for mc install, capture location
+
+// show some cool shit on the screen
+// give them the JSON file so they have the links
+
+// download the mods
+
+// kick off the game
