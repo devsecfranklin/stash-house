0d8f38966400eabeb24474ab5751e57b90516ad8
franklin <2730246+devsecfranklin@users.noreply.github.com>
2025-10-05 20:25:57 -0600

    work on installer script

--- bin/common.sh
+++ bin/common.sh
@@ -49,3 +49,55 @@ function check_if_root {
     log_success "You are NOT the root user."
   fi
 }
+
+# Check if we are inside a container
+CONTAINER=false
+function check_container() {
+  if [ -f /.dockerenv ]; then
+    log_warn "Containerized build environment"
+    CONTAINER=true
+  else
+    log_info "NOT a containerized build environment."
+  fi
+}
+
+function check_installed() {
+  if command -v "$1" &>/dev/null; then
+    log_success "Found command: ${1}"
+    return 0
+  else
+    log_error "Command not found: ${1}"
+    return 1
+  fi
+}
+
+PRIV_CMD="sudo"
+function install_debian() {
+  # Container package installs will fail unless you do an initial update, the upgrade is optional
+  if [ "${CONTAINER}" = true ]; then
+    log_info "Upgrading container packages"
+    sudo apt-get update && apt-get upgrade -y
+    sudo apt-get autoremove -y
+  fi
+
+  for i in "${DEB_PKG[@]}"; do
+    if [ $(dpkg-query -W -f='${Status}' ${i} 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+      log_warn "Installing ${i} since it is not found."
+      # If we are in a container there is no sudo in Debian
+      if [ "${CONTAINER}" = true ]; then
+        $PRIV_CMD apt-get --yes install "${i}"
+        $PRIV_CMD apt-get autoremove -y
+      else
+        $PRIV_CMD apt-get install "${i}" -y
+        $PRIV_CMD apt-get autoremove -y
+      fi
+    else
+      log_info "found package: ${i}"
+    fi
+  done
+
+  if ! check_installed dircolors && [ ! -d "${HOME}/.dircolors" ]; then
+    dircolors -p >~/.dircolors
+    log_warn "Updating the dircolors configuration."
+  fi
+}
