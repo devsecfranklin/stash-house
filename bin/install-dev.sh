f47dc749da8358779b23aafa230311b12244f9ce
Franklin D. <2730246+devsecfranklin@users.noreply.github.com>
2025-10-25 07:39:53 -0600

    update the slide deck (#24)
    * update the slide deck
    
    * test the demo scripts
    
    * slide cleanup

--- bin/install-dev.sh
+++ bin/install-dev.sh
@@ -12,16 +12,16 @@ DEB_PKG=(curl git gnupg2 keyringer make pass podman-compose)
 #SCRIPT_DIR="${0%/*}" # echo "$SCRIPT_DIR"
 
 if [ -f "./common.sh" ]; then
-  source "./common.sh"
+	source "./common.sh"
 else
-  echo -e "${LRED}can not find common.sh, run this script from the bin/ directory.${NC}"
-  exit 1
+	echo -e "${LRED}can not find common.sh, run this script from the bin/ directory.${NC}"
+	exit 1
 fi
 
 function main() {
-  log_header "Installing Development Environment for the Stash House"
-  check_python_version
-  install_debian
+	log_header "Installing Development Environment for the Stash House"
+	check_python_version
+	install_debian
 }
 
 main "$@"
--- demo/01_locate/README.md
+++ demo/01_locate/README.md
@@ -1,4 +1,8 @@
 # 01 Locate
 
+- NOTE: This section of the demo does contain inert keys and credentials.
+  - These may trigger alerts in your build pipeline
+  - Do NOT store your credentials in revision control. NO. BAD.
 - Use the `dirs.txt` and `files.txt` files to direct the tool on where to look.
-- We are not trying to create a script kiddie scanning tool.
+- This tool is intentionally limited in scope and capabillities.
+  - We are not trying to create a script kiddie scanning tool.
